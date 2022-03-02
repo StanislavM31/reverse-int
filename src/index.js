7b245fc6084722286150571386546adbc2c467f7
Stanislav Minko <minkostas@gmail.com>
2022-03-02 04:01:56 +0300

    sucсessful commit! =)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+  let size;
+  let x;
+  x=n+''; //string
+  size = x.length;
+  x=x.split(''); //object array
+  x=x.reverse(); //revert array
+  let res = x.join('');
+  if(x[size-1] == '-'){
+    x.pop();
+    res = x.join('');
+    return res
+  }
+  return res;
 }
